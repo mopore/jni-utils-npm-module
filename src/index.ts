3543cfb278076b27446945e17d5279af9378574e
mopore <jni@mopore.org>
2020-10-01 20:33:28 +0200

    Namenormalizer actually exposed.

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import * as JniCrypto from "./jni/utils/crypto/JniCrypto";
 import * as JniCommon from "./jni/utils/common/JniCommon";
+import { NameNormalizer } from "./jni/utils/common/NameNormalizer";
 
 export {JniCrypto};
 export {JniCommon};
+export {NameNormalizer}
